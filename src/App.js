32d7364f0d250598c60d949999c45fd5368853e3
DFalque <falque@me.com>
2021-08-30 17:17:23 +0200

    change the store component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import NewNote from "./components/NewNote";
 import Notes from "./components/Notes";
 import FilterNote from "./components/FilterNotes";
 
-//TODO Para tener diferentes reducers en un Store
-/*const reducer = combineReducers({
-  reducerUno,
-});*/
-
 const App = () => {
   return (
     <div className="App">
